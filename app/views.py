bd399c6449d4551dac39ca4ba1082cfbc68a36a6
saidansaf <afzalxonovsaidansaf@gmail.com>
2026-04-21 18:38:19 +0500

    hammasi

--- app/views.py
+++ app/views.py
@@ -6,6 +6,6 @@ from django.http import HttpResponse
 
 def rendering(request):
     users=[
-        #{'ism':'Saidansaf','familiya':'Afzalxonov','yosh':'11'},
+        {'ism':'Saidansaf','familiya':'Afzalxonov','yosh':'11'},
     ]
     return render(request,'app/index.html',context={"users":users})
--- project/settings.py
+++ project/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'app',
 ]
 
@@ -115,10 +114,19 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+MEDIA_ROOT=BASE_DIR/'media'
+MEDIA_URL='/media/'
 
 STATIC_URL = 'static/'
+import os
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
