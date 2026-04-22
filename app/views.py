c509cda83de130aef5d8a6cb28c894859cc09d86
saidansaf <afzalxonovsaidansaf@gmail.com>
2026-04-22 19:04:25 +0500

    salom

--- app/views.py
+++ app/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
 
-from django.http import HttpResponse
+def start(request):
+    users=models.User.objects.all() 
+    return render(request, 'app/index.html',context={'users':users})
 
-def rendering(request):
-    users=[
-        {'ism':'Saidansaf','familiya':'Afzalxonov','yosh':'11'},
-    ]
-    return render(request,'app/index.html',context={"users":users})
+def user_view(request,user_id):
+    user=models.User.objects.get(pk=user_id)
+    return render(request,'app/user_view.html',{'user':user})
