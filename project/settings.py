39c96c0fe09316a002140eec25eb93694bc5a939
Saidakbar2013afzalxonov <afzalxonovsaidakbar@gmail.com>
2026-05-01 13:02:42 +0500

    postgresql_bilan

--- project/settings.py
+++ project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'users',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
